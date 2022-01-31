8a517cdf979e939b01f752898e0998e6be92ee62
dipaschoal <di.paschoalini@gmail.com>
2022-01-31 19:44:03 -0300

    Ajustes mysql

--- node/index.js
+++ node/index.js
@@ -1,18 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
-// const config = {
-//     host: 'db',
-//     user: 'root',
-//     password: 'root',
-//     database: 'nodedb'
-// };
-// const mysql = require('mysql')
-// const connection = mysql.createConnection(config)
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
 
-// const sql = `INSERT INTO people (name) values('Diogo')`
-// connection.query(sql)
-// connection.end()
+const sql_create = `CREATE TABLE IF NOT EXISTS people (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    name VARCHAR(255) NOT NULL)  ENGINE=INNODB`
+
+const sql = `INSERT INTO people (name) values('Diogo')`
+
+connection.query(sql_create)
+
+connection.query(sql)
+connection.end()
 
 app.get('/',(req,res) => {
     res.send('<h1>Full Cycle Rocks!</h1>')
