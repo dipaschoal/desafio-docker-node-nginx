777848307e67225e32ab9c3cedd32c122a3c5f8b
dipaschoal <di.paschoalini@gmail.com>
2022-01-31 19:29:59 -0300

    Ajustes node_modules

--- node/index.js
+++ node/index.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const config = {
-    host: 'db',
-    user: 'root',
-    password: 'root',
-    database: 'nodedb'
-};
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
+// const config = {
+//     host: 'db',
+//     user: 'root',
+//     password: 'root',
+//     database: 'nodedb'
+// };
+// const mysql = require('mysql')
+// const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people (name) values('Diogo')`
-connection.query(sql)
-connection.end()
+// const sql = `INSERT INTO people (name) values('Diogo')`
+// connection.query(sql)
+// connection.end()
 
 app.get('/',(req,res) => {
     res.send('<h1>Full Cycle Rocks!</h1>')
