877a99265b90af5e6278c0758082f4926fa3d4cb
dipaschoal <di.paschoalini@gmail.com>
2022-02-01 16:50:23 -0300

    commit leitura da tabela

--- node/index.js
+++ node/index.js
@@ -5,24 +5,43 @@ const config = {
     host: 'db',
     user: 'root',
     password: 'root',
-    database: 'nodedb'
-};
+    database: 'nodedb',
+    charset: 'utf8'
+  }
+
 const mysql = require('mysql')
-const connection = mysql.createConnection(config)
+const con = mysql.createConnection(config)
 
 const sql_create = `CREATE TABLE IF NOT EXISTS people (
                     id INT AUTO_INCREMENT PRIMARY KEY,
-                    name VARCHAR(255) NOT NULL)  ENGINE=INNODB`
+                    name VARCHAR(255) NOT NULL,
+                    data VARCHAR(255)) ENGINE=INNODB`
 
-const sql = `INSERT INTO people (name) values('Diogo')`
+const sql_insert = `INSERT INTO people (name, data) values('Diogo', NOW())`
 
-connection.query(sql_create)
+const sql_list = `SELECT * FROM people ORDER BY id DESC`
 
-connection.query(sql)
-connection.end()
+var table
+
+con.query(sql_create)
+con.query(sql_insert)
 
 app.get('/',(req,res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
+
+    con.query(sql_list, function (err, result) {
+        if (err) throw err        
+
+        this.table = ""
+        for(var i=0; i<result.length; i++){
+            this.table+='<tr><td>'+result[i].id+'</td><td>'+result[i].name+'</td><td>'+result[i].data+'</td></tr>'
+        }
+
+        this.table = '<h1>Full Cycle Rocks!</h1><table><tr><th>id</th><th>name</th><th>data</th></tr>'+this.table+'</table>'
+
+        console.log(this.table)
+        res.send(this.table)
+        con.end()
+    })
 })
 
 app.listen(port, ()=>{
